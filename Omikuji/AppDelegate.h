7c7a291f1fd5faa7028e3223fc7ee06d464e8270
okadaakihito <akihito.okada@gmail.com>
2013-10-28 20:23:57 +0900

    Added Omikuji App

---
+++ Omikuji/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Omikuji
+//
+//  Created by Satoshi Horikoshi on 2013/08/03.
+//  Copyright (c) 2013年 Satoshi Horikoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Omikuji/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Omikuji
+//
+//  Created by Satoshi Horikoshi on 2013/08/03.
+//  Copyright (c) 2013年 Satoshi Horikoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
